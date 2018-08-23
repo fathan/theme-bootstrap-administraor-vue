6a95d6c94a4419d68836d272825389708df686d3
fathan <kreasi.fathan@gmail.com>
2018-08-23 09:57:48 +0700

    Update file production

---
+++ dist/precache-manifest.f8a8030d2b3826b6c7979bcde020b510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "18fc670adb73e4a4abdc",
+    "url": "/js/chunk-vendors.2fa14025.js"
+  },
+  {
+    "revision": "ca066c545a61bef4bf8d",
+    "url": "/js/app.df1f619f.js"
+  },
+  {
+    "revision": "8848da168143d060c97ca653c75bcebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18fc670adb73e4a4abdc",
+    "url": "/css/chunk-vendors.3ff6acff.css"
+  },
+  {
+    "revision": "ca066c545a61bef4bf8d",
+    "url": "/css/app.04f5af55.css"
+  }
+];
